39b51a9d9f2ef0895ed5545846e6440c41465c52
gema <gemaevelinu@gmail.com>
2024-05-14 18:41:47 -0300

    Intercambio inicial de proyecto

---
+++ ArrayUtils.java
@@ -0,0 +1,95 @@
+
+/**
+ * Clase ArrayUtils: contiene implementaciones de rutinas fundamentales
+ * sobre arreglos.
+ * 
+ * @author N. Aguirre
+ * @version 0.1
+ */
+public class ArrayUtils
+{
+    
+    /**
+     * Comprueba si un elemento (entero) pertenece a un arreglo.
+     */
+    public boolean pertenece(int elem, int[] arreglo) {
+        if (arreglo == null)
+            throw new IllegalArgumentException("Arreglo nulo");
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return false;
+    }
+    
+    /**
+     * Calcula la suma de los elementos de un arreglo de enteros
+     */
+    public int suma(int[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return 0;
+    }
+    
+    /**
+     * Calcula el valor mínimo de un arreglo de reales
+     */
+    public float minimo(float[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return 0;
+    }
+    
+    /**
+     * Calcula el valor máximo de un arreglo de reales
+     */
+    public float maximo(float[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return 0;
+    }
+    
+    
+    /**
+     * Calcula el valor promedio de un arreglo de reales
+     */
+    public float promedio(float[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return 0;
+    }
+    
+    /**
+     * Retorna un arreglo con los primeros n números de Fibonacci
+     */
+    public int[] fibSequence(int n) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return null;
+    }
+    
+    /**
+     * Calcula la mediana de un arreglo de reales
+     */
+    public float mediana(float[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return 0;
+    }
+    
+    /**
+     * Comprueba si dos arreglos de enteros son iguales
+     */
+    public boolean iguales(int[] arr1, int[] arr2) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return false;
+    }
+    
+    /**
+     * Comprueba si un arreglo tiene valores repetidos
+     */
+    public boolean tieneRepetidos(int[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return false;
+    }
+    
+    /**
+     * Comprueba si un arreglo tiene dos valores cuya suma dé cero.
+     */
+    public boolean tieneOpuestos(float[] arreglo) {
+        //TODO: Completar la implementación, reemplazando la línea siguiente
+        return false;
+    }
+    
+}
